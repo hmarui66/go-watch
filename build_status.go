f1f3f4df4aa0af6707ab1a7df5a740b3676b3238
hmarui66 <hmarui66@gmail.com>
2019-03-05 15:59:38 +0900

    delete error handling to create/delete error file

--- build_status.go
+++ build_status.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"log"
 	"os"
 	"sync"
 )
@@ -14,15 +13,11 @@ type buildStatus struct {
 func (bs *buildStatus) error() {
 	bs.mux.Lock()
 	defer bs.mux.Unlock()
-	if _, err := os.Create(bs.errFile); err != nil {
-		log.Printf(`failed to create error file => %+v`, err)
-	}
+	os.Create(bs.errFile)
 }
 
 func (bs *buildStatus) success() {
 	bs.mux.Lock()
 	defer bs.mux.Unlock()
-	if err := os.Remove(bs.errFile); err != nil {
-		log.Printf(`failed to remove error file => %+v`, err)
-	}
+	os.Remove(bs.errFile)
 }
